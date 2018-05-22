2754c0875c07a766ce2bbe67b5b51018b6b7bb9e
jackstellwagen <jstellwagen19@students.polytechnic.org>
2018-05-22 16:56:24 -0700

    Got control dependencies to workgit add test_program.py

--- test_program.py
+++ test_program.py
@@ -18,7 +18,7 @@ import time
 
 
 batch_size = 35
-num_steps = 200
+num_steps = 800
 vector_dim = 200
 tf.reset_default_graph()
 
@@ -31,7 +31,7 @@ def noise_array():
 
 
 perfect_data = np.indices((500,27998))[1]
-sin = lambda t: np.sin(1/np.random.randint(900,1500) *t)
+sin = lambda t: np.sin(1/np.random.randint(600,1500) *t)
 data = np.apply_along_axis(sin, 1, perfect_data) + noise_array()
 
 
@@ -39,18 +39,16 @@ noise = lambda t: np.random.normal(scale = 0.05) * t
 vfunc = np.vectorize(noise)
 #data = vfunc(data)
 
-#data = vfunc(perfect_data, np.random.randint(100,1500)
-#def data_func(perfect_data):
 
 
 
 #vfunc = np.vectorize(sin)
 
 #perfect_data = np.zeros(shape=(500,27998))
-"""
-perfect_data[:,8000:16000] = 0.7
-perfect_data[:,16000:] = -0.7
 
+#perfect_data[:,8000:16000] = 0.7
+#perfect_data[:,16000:] = -0.7
+"""
 perfect_data[0:250,:12500] = 0.7
 perfect_data[250:, 12500:] = -0.2 
 
@@ -60,9 +58,8 @@ for i in range(len(sin_wave)):
 perfect_data[:,:] = sin_wave
 print(perfect_data.shape, "sin_wave")
 np.save("perfect_data.npy", perfect_data)    
-
-data = perfect_data + noise_array()
 """
+#data = perfect_data + noise_array()
 
 n_samp, n_input = data.shape
 print(data.shape)
@@ -73,6 +70,12 @@ with tf.variable_scope('Generator', reuse=True):
      bconv2 = tf.Variable(tf.zeros([32]), name ="bconv2")
      bconv3 = tf.Variable(tf.zeros([16]), name ="bconv3")
      bconv4 = tf.Variable(tf.zeros([8]), name ="bconv4")
+     bconv5 = tf.Variable(tf.zeros([1]), name ="bconv5")
+     wconv1 = tf.Variable(tf.truncated_normal([30,64,64]), name ="wconv1")
+     wconv2 = tf.Variable(tf.truncated_normal([30,32,64]), name ="wconv2")
+     wconv3 = tf.Variable(tf.truncated_normal([30,16,32]), name ="wconv3")
+     wconv4 = tf.Variable(tf.truncated_normal([30,8,16]), name ="wconv4")
+     wconv5 = tf.Variable(tf.truncated_normal([54,1,8]), name ="wconv5")
 
 def generator(x, isTrain=True,reuse=False, batch_size=batch_size):
     with tf.variable_scope('Generator', reuse=reuse):
@@ -82,52 +85,52 @@ def generator(x, isTrain=True,reuse=False, batch_size=batch_size):
         x =tf.layers.batch_normalization(x,momentum=0.9, training =isTrain, epsilon=0.00001)#,name = "g_bn_d1")
         x = tf.nn.relu(x)
         
-        wconv1 = tf.Variable(tf.truncated_normal([30,64,64]), name ="wconv1")
+        #wconv1 = tf.Variable(tf.truncated_normal([30,64,64]), name ="wconv1")
         conv1 = tf.contrib.nn.conv1d_transpose(x,wconv1,[batch_size,1035,64], stride=3,padding ="SAME")
         #bconv1 = tf.Variable(tf.zeros([64]), name ="bconv1")
         conv1 = tf.nn.bias_add(conv1, bconv1)
         conv1 = tf.nn.relu(tf.layers.batch_normalization(conv1,momentum=0.9, training =isTrain,epsilon=0.00001))#,name="g_bn_conv1"))
         print(conv1.get_shape(),"conv1")     
 
-        wconv2 = tf.Variable(tf.truncated_normal([30,32,64]), name ="wconv2")
+        #wconv2 = tf.Variable(tf.truncated_normal([30,32,64]), name ="wconv2")
         conv2 = tf.contrib.nn.conv1d_transpose(conv1, wconv2,[batch_size, 3105,32], stride=3, padding="SAME")
         #bconv2 = tf.Variable(tf.zeros([32]), name ="bconv2")
         conv2 = tf.nn.bias_add(conv2, bconv2)
         conv2 = tf.nn.relu(tf.layers.batch_normalization(conv2,momentum=0.9,training =isTrain,epsilon=0.00001))#,name="g_bn_conv2"))
         print(conv2.get_shape(),"conv2")
 
-        wconv3 = tf.Variable(tf.truncated_normal([30,16,32]), name ="wconv3")
+        #wconv3 = tf.Variable(tf.truncated_normal([30,16,32]), name ="wconv3")
         conv3 = tf.contrib.nn.conv1d_transpose(conv2, wconv3,[batch_size, 9315,16], stride=3, padding="SAME")
         #bconv3 = tf.Variable(tf.zeros([16]), name ="bconv3")
         conv3 = tf.nn.bias_add(conv3, bconv3)
         conv3 = tf.nn.relu(tf.layers.batch_normalization(conv3,momentum=0.9,training =isTrain,epsilon=0.00001))#, name="g_bn_conv3"))
         print(conv3.get_shape(),"conv3")
 
-        wconv4 = tf.Variable(tf.truncated_normal([30,8,16]), name ="wconv4")
+        #wconv4 = tf.Variable(tf.truncated_normal([30,8,16]), name ="wconv4")
         conv4 = tf.contrib.nn.conv1d_transpose(conv3, wconv4, [batch_size, 27945,8], stride=3, padding="SAME")
         #bconv4 = tf.Variable(tf.zeros([8]), name ="bconv4")
         conv4 = tf.nn.bias_add(conv4, bconv4)
         conv4 = tf.nn.relu(tf.layers.batch_normalization(conv4,momentum=0.9,training =isTrain,epsilon=0.0001))#, name="g_bn_conv4"))
         print(conv4.get_shape(),"conv4")
 
-        wconv5 = tf.Variable(tf.truncated_normal([54,1,8]), name ="wconv5")
+        #wconv5 = tf.Variable(tf.truncated_normal([54,1,8]), name ="wconv5")
         conv5 = tf.contrib.nn.conv1d_transpose(conv4, wconv5, [batch_size,27998,1], stride=1, padding="VALID")
-        bconv5 = tf.Variable(tf.zeros([1]), name ="bconv5")
+        #bconv5 = tf.Variable(tf.zeros([1]), name ="bconv5")
         conv5 = tf.nn.bias_add(conv5, bconv5)
         conv5 = tf.layers.batch_normalization(conv5,momentum=0.9,training =isTrain,epsilon=0.00001)
-        out = conv5
+        out = bconv5
         conv5 = tf.nn.tanh(conv5)
         print(conv5.get_shape(),"conv5")
         #conv5 = tf.squeeze(conv5, axis = 2)
         #conv5 = tf.squeeze(conv5, axis = 2)
-        return conv5, out
+        return conv5
 
 def discriminator(x,isTrain=True, reuse=False):
     with tf.variable_scope('Discriminator', reuse=reuse):
         
         #print(x.get_shape(),'i')
         #conv1 = conv1d()
-        conv1 = tf.layers.conv1d(x,4, 30,strides=3, padding = "Same",kernel_initializer = tf.contrib.layers.xavier_initializer_conv2d())
+        conv1 = tf.layers.conv1d(x,4, 30,strides=2, padding = "Same",kernel_initializer = tf.contrib.layers.xavier_initializer_conv2d())
         conv1 = tf.nn.leaky_relu(tf.layers.batch_normalization(conv1, momentum=0.9, training =isTrain, epsilon=0.00001))
         #conv1 = tf.nn.leaky_relu(conv1)
         conv2 = tf.layers.conv1d(conv1, 16, 30,strides =2,padding = "Same", kernel_initializer = tf.contrib.layers.xavier_initializer_conv2d())
@@ -145,11 +148,11 @@ def discriminator(x,isTrain=True, reuse=False):
         dense1 = tf.layers.dense(flatten, 2048,kernel_initializer = tf.contrib.layers.xavier_initializer())
         #out = tf.nn.sigmoid(dense1)
 
-        dense1 = tf.layers.batch_normalization(dense1, momentum=0.9, training =isTrain)
+        dense1 = tf.layers.batch_normalization(dense1, momentum=0.9, training =isTrain, epsilon = 0.00001)#, trainable =False)
         dense1 = tf.nn.leaky_relu(dense1)
         dense2 = tf.layers.dense(dense1, 512,kernel_initializer = tf.contrib.layers.xavier_initializer())
-        dense2 = tf.layers.batch_normalization(dense2, momentum=0.9, training =isTrain)
-        d3 = dense2
+        dense2 = tf.layers.batch_normalization(dense2, momentum=0.9, training =isTrain, epsilon =0.00001)#,trainable =False)
+        #d3 = dense2
         dense2 = tf.nn.leaky_relu(dense2)
         dense3 = tf.layers.dense(dense2, 1,kernel_initializer = tf.contrib.layers.xavier_initializer())
         #out = tf.nn.sigmoid(dense3)
@@ -159,7 +162,7 @@ def discriminator(x,isTrain=True, reuse=False):
         #return dense3
         
         #return out,dense3
-        return out,d3
+        return out,dense3
         """
         conv1 = tf.layers.conv1d(x,8, 30, padding = "Same",kernel_initializer = tf.contrib.layers.xavier_initializer())
         conv1 = tf.nn.tanh(conv1)
@@ -235,7 +238,7 @@ real_image_input = tf.placeholder(tf.float32, shape=[None,27998, 1])
 
 isTrain = tf.placeholder(dtype=tf.bool)
 
-gen_sample, out_test = generator(random_vector, isTrain, batch_size=batch_size)
+gen_sample = generator(random_vector, isTrain, batch_size=batch_size)
 
 disc_real,disc_real_logits = discriminator(real_image_input,isTrain)
 disc_fake,disc_fake_logits = discriminator(gen_sample,isTrain, reuse=True)
@@ -266,31 +269,42 @@ gen_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(
 #extra_update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
 #with tf.control_dependencies(extra_update_ops):
 
-#optimizer_gen = tf.train.AdamOptimizer(learning_rate=0.002, beta1=0.5)
-#optimizer_disc = tf.train.AdamOptimizer(learning_rate=0.002, beta1= 0.5)
+optimizer_gen = tf.train.AdamOptimizer(learning_rate=0.002, beta1=0.5)
+optimizer_disc = tf.train.AdamOptimizer(learning_rate=0.002, beta1= 0.5)
 
 
 gen_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='Generator')
 disc_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope='Discriminator')
 
-gen, ot  = generator(random_vector,batch_size=100,reuse=True)
+#gen  = generator(random_vector,batch_size=100,reuse=True)
 
 #print(gen_vars)
 #extra_update_ops_disc = tf.get_collection(tf.GraphKeys.UPDATE_OPS, scope ="Discriminator")
 #extra_update_ops_gen = tf.get_collection(tf.GraphKeys.UPDATE_OPS, scope ="Generator")
 
-extra_update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+#train_gen = optimizer_gen.minimize(gen_loss, var_list=gen_vars)
+#train_disc = optimizer_gen.minimize(disc_loss, var_list=disc_vars)
+update_ops_gen = tf.get_collection(tf.GraphKeys.UPDATE_OPS, scope = "Generator")
+update_ops_disc = tf.get_collection(tf.GraphKeys.UPDATE_OPS, scope="Discriminator")
+
+with tf.control_dependencies(update_ops_gen):
+     train_gen = optimizer_gen.minimize(gen_loss, var_list=gen_vars)
+
+with tf.control_dependencies(update_ops_disc):
+    train_disc = optimizer_gen.minimize(disc_loss, var_list=disc_vars)
+
 """
-def train(loss):
+def train(lossg, lossd):
 
     optimizer_gen = tf.train.AdamOptimizer(learning_rate=0.002, beta1=0.5)
     optimizer_disc = tf.train.AdamOptimizer(learning_rate=0.002, beta1= 0.5)
 
-    train_g = optimizer_gen.minimize(loss, var_list=gen_vars)
-    train_d = optimizer_disc.minimize(loss, var_list=disc_vars)
+    train_g = optimizer_gen.minimize(lossg, var_list=gen_vars)
+    train_d = optimizer_disc.minimize(lossd, var_list=disc_vars)
     with tf.control_dependencies([train_g, train_d]):
         return tf.no_op(name="train")
-"""
+train_op = train(gen_loss,disc_loss)
+
 def traind(dloss):
 
     #optimizer_gen = tf.train.AdamOptimizer(learning_rate=0.002, beta1=0.5)
@@ -310,10 +324,9 @@ def traing(gloss):
     #train_d = optimizer_disc.minimize(loss, var_list=disc_vars)
     with tf.control_dependencies(train_g):
         return tf.no_op(name="traing")
+#train_gen = train(gen_loss)
+#train_disc = train(disc_loss)
 
-train_gen = traing(gen_loss)
-train_disc = traind(disc_loss)
-"""
 with tf.control_dependencies(extra_update_ops_gen):
      train_gen = optimizer_gen.minimize(gen_loss)#, var_list=gen_vars)
 
@@ -328,10 +341,11 @@ init = tf.global_variables_initializer()
 						
 saver = tf.train.Saver()
 with tf.Session() as sess:
-    start = time.time()
+    #start = time.time()
     sess.run(init)
     #dl = 1
     #gl = 1
+    saver.restore(sess, "/home/jack/caltech_research/cell_data_GAN_network/cell_data_GAN_trained.ckpt")
     for i in range(1, num_steps+1):
         if i% 50 == 0:# and i!=500:
             #print("New Data")
@@ -339,11 +353,13 @@ with tf.Session() as sess:
              #data = perfect_data + noise_array()
              start = time.time()
              data = np.apply_along_axis(sin, 1, perfect_data) + noise_array()
+             #data = perfect_data+noise_array()
              print(time.time()-start, "time")
         sample = np.random.randint(n_samp, size=batch_size)
         epoch_x = data[sample,:]
         epoch_x = np.reshape(epoch_x, newshape=[-1, 27998, 1])
         z = np.random.uniform(-1.0, 1.0, size=[batch_size, vector_dim])
+        #dl,_,dlr,dlf, gl = sess.run([disc_loss,train_op,disc_loss_real, disc_loss_fake,gen_loss], feed_dict = {real_image_input:epoch_x, random_vector:z, isTrain:True})
        	dl,_,dlr,dlf = sess.run([disc_loss,train_disc,disc_loss_real, disc_loss_fake], feed_dict = {real_image_input:epoch_x, random_vector:z, isTrain:True})
         gl, _ = sess.run([gen_loss,train_gen], feed_dict = {random_vector:z,isTrain:True})
         """
@@ -377,25 +393,19 @@ with tf.Session() as sess:
             print('Step %i: Generator Loss: %f, Discriminator Loss: %f' % (i, gl, dl))
             print("DLR:",dlr,",", "DLF:", dlf)
     save_path = saver.save(sess, "/home/jack/caltech_research/cell_data_GAN_network/cell_data_GAN_trained.ckpt")
-    finish = time.time() -start
-    out = open("timing.txt",'w')
-    var = str(num_steps) + ':' + str(finish)
-    out.write(var)
-    print(finish)
-    out.close()
 
     sample = np.random.randint(n_samp, size=batch_size)
     epoch_x = data[sample,:]
     epoch_x = np.reshape(epoch_x, newshape=[-1, 27998,1])
     print(epoch_x)
-    z = np.random.uniform(-1., 1., size=[100, vector_dim])
+    z = np.random.uniform(-1., 1., size=[batch_size, vector_dim])
     #gen = generator(random_vector,batch_size=100,reuse=True)
-    samp, samp2 = sess.run([ot,gen], feed_dict={random_vector: z})
+    samp  = sess.run(gen_sample, feed_dict={random_vector: z, isTrain:False})
     
-    g = sess.run(disc_real_logits, feed_dict={real_image_input: epoch_x, isTrain:False})
+    g = sess.run(disc_real, feed_dict={real_image_input: epoch_x, isTrain:True})
     o = sess.run(disc_real, feed_dict={real_image_input: samp, isTrain:False})
     #o = epoch_x
     np.save("disc_output_fake.npy",o)    
     np.save("disc_output.npy", g)
     np.save("generator_output.npy", samp)
-    np.save("generator_output2.npy", samp2)
+    #np.save("generator_output2.npy", samp2)
